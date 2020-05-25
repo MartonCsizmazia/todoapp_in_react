240ebf4b8d6a9096971a7b2de246bcd8314e22ed
MartonCsizmazia <csmarton93@gmail.com>
2020-05-25 22:36:24 +0200

    Header added to todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import Header from './Header';
 
 export default function TodoList(props) {
+    const {mode, query} = props.data;
+    const {addNew,setSearchQuery} = props.actions;
     return (
         <div className="container">
             <div className="row">
-
+                <Header {...{addNew, mode, query, setSearchQuery}}/>
             </div>
         </div>
     );
