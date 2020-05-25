8f26ca96ae7e10d76c1a1bb6df4f4b6d09730a70
MartonCsizmazia <csmarton93@gmail.com>
2020-05-25 22:50:42 +0200

    start to implement searching feature

--- src/components/InputWrapper.js
+++ src/components/InputWrapper.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import InputBox from './InputBox';
-import { MODE_CREATE} from './mode';
+import SearchBox from './SearchBox';
+import {MODE_SEARCH, MODE_CREATE} from './mode';
 
 export default function InputWrapper(props) {
-    const {mode, addNew} = props;
+    const {mode, addNew, query, setSearchQuery} = props;
 
     switch (mode) {
         case MODE_CREATE:
             return <InputBox {...{addNew}}/>;
 
+        case MODE_SEARCH:
+            return <SearchBox {...{query, setSearchQuery}}/>;
 
         default:
             return null;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SearchBox() {
+    return (
+        <input
+            type="text" autoFocus
+            className="form-control search"
+            placeholder="Search"
+        />
+    );
+}
